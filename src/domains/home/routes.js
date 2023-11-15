4134ed3d2b7f64d4bffef0fa337671cf4d4ad63a
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-16 05:41:04 +0900

    feat: homeRouter.js 생성 및 mainRouter에 연결
    Ref: https://github.com/sw-study-lab/sw-shop/issues/7

---
+++ src/domains/home/routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const homeRouter = express.Router();
+
+export default homeRouter;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRouter from "./domains/user/routes";
 import productRouter from "./domains/product/routes";
+import homeRouter from "./domains/home/routes";
 
 const router = express.Router();
 
+router.use(homeRouter);
 router.use("/users", userRouter);
 router.use("/products", productRouter);
 
