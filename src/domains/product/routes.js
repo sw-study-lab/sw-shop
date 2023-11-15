4328a41f074a97a244526169fb94a5137ced83e3
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-16 05:37:20 +0900

    feat: productRouter.js 생성 및 mainRouter에 연결
    Ref: https://github.com/sw-study-lab/sw-shop/issues/7

---
+++ src/domains/product/routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const productRouter = express.Router();
+
+export default productRouter;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./domains/user/routes";
+import productRouter from "./domains/product/routes";
 
 const router = express.Router();
 
 router.use("/users", userRouter);
+router.use("/products", productRouter);
 
 export default router;
