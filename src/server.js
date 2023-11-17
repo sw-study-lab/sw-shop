35251303ce5f0bfa3843e1cca22971e7e2ba6322
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-17 10:24:47 +0900

    feat: session 기능 추가 (#24)
    Ref: https://github.com/sw-study-lab/sw-shop/issues/19

--- src/server.js
+++ src/server.js
@@ -3,10 +3,23 @@ import mainRoutes from "./routes";
 import morgan from "morgan";
 import { handleNotFound } from "./middlewares/errorHandlers/notFoundHandler";
 import { errorHandler } from "./middlewares/errorHandlers";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 90000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
 app.use(mainRoutes);
 app.use(handleNotFound);
 app.use(errorHandler);
