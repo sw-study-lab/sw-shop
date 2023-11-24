c3f10f220e0bc48b572352c9c5e75730b24d9db7
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-24 09:57:40 +0900

    feat: userRouter.js에서 authRouter코드 분리

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -11,18 +11,6 @@ const userRouter = express.Router();
 
 userRouter.route("/").get(getUsers).post(postUser);
 
-userRouter
-  .route("/join")
-  .get((req, res, next) => res.send("회원가입 페이지 불러오기"))
-  .post((req, res, next) => res.send("회원가입 post 요청하기"));
-
-userRouter
-  .route("/login")
-  .get((req, res, next) => res.send("login 페이지 불러오기"))
-  .post((req, res, next) => res.send("login post 요청하기"));
-
-userRouter.route("/logout").get((req, res, next) => res.send("로그아웃 요청"));
-
 userRouter.route("/:id").get(getOne).put(updateOne).delete(removeOne);
 
 export default userRouter;
